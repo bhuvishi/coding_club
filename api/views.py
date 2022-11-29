c1ad55735cf2955d238f5f40ae7f2894a7736640
Sujeet BANSAL <sujeetba@amazon.com>
2022-11-29 03:49:21 +0000

    initial commit

---
+++ api/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework import generics
+from api.models import Student
+from api.serializers import StudentSerializer
+
+
+class StudentList(generics.ListCreateAPIView):
+  queryset = Student.objects.all()
+  serializer_class = StudentSerializer
+
+
+class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Student.objects.all()
+  serializer_class = StudentSerializer
